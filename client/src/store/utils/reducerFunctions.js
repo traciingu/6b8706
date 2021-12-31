d0981d248f1ba8733bd5aa1d35a384cd5cb67d08
traciingu <traciinguyen@hotmail.com>
2021-12-31 17:36:11 -0500

    BUG-1: Fix func to immutably update state

--- client/src/store/utils/reducerFunctions.js
+++ client/src/store/utils/reducerFunctions.js
@@ -13,9 +13,14 @@ export const addMessageToStore = (state, payload) => {
 
   return state.map((convo) => {
     if (convo.id === message.conversationId) {
-      convo.messages.push(message);
       convo.latestMessageText = message.text;
-      return convo;
+      return {
+        ...convo,
+        messages: [
+          message,
+          ...convo.messages
+        ]
+      };
     } else {
       return convo;
     }
