46c37323e898c5167167052b829d348b8cff82e2
traciingu <traciinguyen@hotmail.com>
2022-01-10 15:20:31 -0500

    FEATURE-1: Changed at() to pop() for testing

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -20,7 +20,7 @@ const Messages = (props) => {
   useEffect(() => {
     setLastReadMessage((messages && messages.filter(message => {
       return message.readReceipt === true && message.senderId === userId;
-    }).at(-1)) || {});
+    }).pop()) || {});
   }, [messages]);
 
   return (
